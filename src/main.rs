53ba5d236dfba77892318e30e1d3405dd3aa4733
Bryan de Jong <bryandejong99@gmail.com>
2023-09-26 12:34:27 +0100

    feat: add rotating cube

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-use bevy::pbr::CascadeShadowConfig;
+use std::f32::consts::PI;
+use bevy::pbr::CascadeShadowConfigBuilder;
 use bevy::prelude::*;
 use bevy::render::camera::ScalingMode;
+
+#[derive(Component)]
+struct Rotator;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, rotate_system)
         .run();
+}
+
+fn rotate_system(mut query: Query<&mut Transform, With<Rotator>>, time: Res<Time>) {
+    for mut transform in &mut query {
+        transform.rotate_y(time.delta_seconds() * 1.0)
+    }
 }
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     commands.spawn(PbrBundle {
         mesh: meshes.add(shape::Plane::from_size(5.0).into()),
@@ -20,15 +31,40 @@ fn setup(
         ..default()
     });
 
-    commands.spawn(DirectionalLightBundle {
-        directional_light: DirectionalLight::default(),
-        cascade_shadow_config: CascadeShadowConfig::default(),
+    commands.spawn((PbrBundle {
+        mesh: meshes.add(shape::Box::default().into()),
+        material: materials.add(StandardMaterial::from(Color::rgb(0.6, 0.2, 0.2)).into()),
+        transform: Transform::from_xyz(1.0, 0.0, 0.0),
         ..default()
-    });
+    }, Rotator));
 
     commands.insert_resource(AmbientLight {
         color: Color::rgb(1.0, 1.0, 1.0),
-        brightness: 0.4,
+        brightness: 0.1,
+    });
+
+    // directional 'sun' light
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            illuminance: 20000.0,
+            ..default()
+        },
+        transform: Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-PI / 4.),
+            ..default()
+        },
+        // The default cascade config is designed to handle large scenes.
+        // As this example has a much smaller world, we can tighten the shadow
+        // bounds for better visual quality.
+        cascade_shadow_config: CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 4.0,
+            maximum_distance: 10.0,
+            ..default()
+        }
+            .into(),
+        ..default()
     });
 
     commands.spawn(Camera3dBundle {
